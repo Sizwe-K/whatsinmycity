fcd02ac9acad59588a4c7cf6589b4cb30c86683c
Melusi Khumalo <sizwekhumalo.602@gmail.com>
2026-08-31 14:05:02 +0200

    Added database helper class & updated ignore rules

---
+++ Frontend/What'sInMyCity/What'sInMyCity/Data/DatabaseHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace What_sInMyCity.Data
+{
+    public static class DatabaseHelper
+    {
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["WhatsInMyCityConnection"].ConnectionString;
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+
+        }
+    }
+    
+}
